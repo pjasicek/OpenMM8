afce6d742825e2e4d5d5f9b18f0a488997f702b0
Petr Jasicek <pjasicek@gmail.com>
2017-12-19 00:45:48 +0100

    Implemented Compas. Moved UI scripts to separate folder.

---
+++ Assets/OpenMM8/Scripts/UI/Compas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compas : MonoBehaviour 
+{
+    private Transform m_PlayerTransform;
+    private RawImage m_CompasImage;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_CompasImage = GetComponent<RawImage>();
+        m_PlayerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect rect = m_CompasImage.uvRect;
+        rect.x = (m_PlayerTransform.eulerAngles.y - 30) / 360.0f;
+
+        m_CompasImage.uvRect = rect;
+    }
+}
--- Assets/OpenMM8/Scripts/UI/Minimap.cs
+++ Assets/OpenMM8/Scripts/UI/Minimap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TestOffset : MonoBehaviour 
+public class Minimap : MonoBehaviour 
 {
     public Vector2 m_MinimapScale = new Vector2(0.2f, 0.2f);
 
